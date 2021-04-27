f6c46c90647cb0966c95812a4e00ec19c634d985
CaoZ <caoz@rock-it.org>
2021-04-27 16:38:36 +0800

    Some changes.

--- wp-includes/js/util.js
+++ wp-includes/js/util.js
@@ -1,5 +1,4 @@
 function initialVideo(id) {
-
     var aOrigin = jQuery('#' + id);
     var newContentID = 'highslide-html-' + id;
     var newPlayerID = 'player-' + id;
@@ -15,16 +14,26 @@ function initialVideo(id) {
                 </ul>
             </div>
             <div class="highslide-body">
-                <div id="${newPlayerID}" style="display: block; width: 480px; height: 360px; margin: auto">
-                    <video controls style="outline: none">
-                        <source src="${aOrigin.attr('href')}">
-                    </video>
+                <div id="${newPlayerID}" style="width: 480px; height: 360px; margin: auto">
+                    <video width="480" height="360" style="outline: none" src="${aOrigin.attr('href')}"></video>
                 </div>
             </div>
         </div>
     `;
 
-    jQuery('body').append(jQuery.parseHTML(html));
+    jQuery(html).appendTo('body');
+
+    hs.Expander.prototype.onAfterExpand = function (sender) {
+        setTimeout(function() {
+            var video = jQuery(sender.content).find('video');
+            video.trigger('play');
+            
+            setTimeout(function() {
+                video.prop('controls', true);
+            }, 1000);
+
+        }, 100);
+    };
 
     aOrigin.click(function() {
         return hs.htmlExpand(aOrigin[0], {
@@ -33,9 +42,27 @@ function initialVideo(id) {
             wrapperClassName: 'draggable-header no-footer'
         });
     });
-
-    aOrigin.click(function() {
-        jQuery('#' + newPlayerID).children('video')[0].play();
-    });
-
 }
+
+//////////////////////////////
+
+var _hmt = _hmt || [];
+
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8785f875403fa16ddd737c1133a21784";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+(function() {
+    var ga = document.createElement('script');
+    ga.src = "https://www.googletagmanager.com/gtag/js?id=G-HT4F9QQ6KY";
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-HT4F9QQ6KY');
